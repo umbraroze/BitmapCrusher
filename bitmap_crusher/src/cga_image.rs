bfd23e4a30eef3f1ae1bcb43d757a4e43ece2be8
Rose Midford <wwwwolf@iki.fi>
2019-10-26 19:33:29 +0300

    Making this stuff more rustic

---
+++ bitmap_crusher/src/cga_image.rs
@@ -0,0 +1,119 @@
+use std::fs::File;
+use std::fmt::{self, Display, Formatter};
+
+pub const PALETTE: [Colour; 16] = [
+	Colour(0x00,0x00,0x00),  // 0 black
+	Colour(0x00,0x00,0xAA),  // 1 blue
+	Colour(0x00,0xAA,0x00),  // 2 green
+	Colour(0x00,0xAA,0xAA),  // 3 cyan
+	Colour(0xAA,0x00,0x00),  // 4 red
+	Colour(0xAA,0x00,0xAA),  // 5 magenta
+	Colour(0xAA,0x55,0x00),  // 6 brown
+	Colour(0xAA,0xAA,0xAA),  // 7 light gray
+	Colour(0x55,0x55,0x55),  // 8 dark gray
+	Colour(0x55,0x55,0xFF),  // 9 light blue
+	Colour(0x55,0xFF,0x55),  // 10 light green
+	Colour(0x55,0xFF,0xFF),  // 11 light cyan
+	Colour(0xFF,0x55,0x55),  // 12 light red
+	Colour(0xFF,0x55,0xFF),  // 13 light magenta
+	Colour(0xFF,0xFF,0x55),  // 14 yellow
+	Colour(0xFF,0xFF,0xFF)   // 15 white
+];
+pub const MODES: [Mode; 4] = [
+	Mode( 0, 2, 4, 6), // P0Lo: black, green, red, brown 
+	Mode( 0,10,12,14), // P0Hi: black, lt.green, lt.red, yellow
+	Mode( 0, 3, 5, 7), // P1Lo: black, cyan, magenta, lt.gray
+	Mode( 0,11,13,15)  // P1Hi: black, lt.cyan, lt.magenta, white
+];
+pub enum GraphicsMode {
+	P0LO, P0HI, P1LO, P1HI
+}
+
+#[derive(Eq,PartialEq,PartialOrd)]
+pub struct Colour(u8,u8,u8);
+impl Display for Colour {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "#{:02X}{:02X}{:02X}", self.0, self.1, self.2)
+    }
+}
+
+pub struct Mode(u8,u8,u8,u8);
+
+pub struct Image {
+	pub width: u32,
+	pub height: u32,
+	pub rgba_data: Vec<u8>,
+	pub p_data: Vec<u8>
+}
+impl Image {
+	fn to_indexed(data: &Vec<u8>) -> Vec<u8> {
+		if data.len() % 4 != 0 {
+			panic!("Data size not divisible by 4 (not RGBA image???)");
+		}
+		let l = data.len()/4;
+		let mut converted = vec![0 as u8; l];
+		for n in 0..l {
+			let dp = n * 4;
+			let r = data[dp];
+			let g = data[dp+1];
+			let b = data[dp+2];
+			// skip "let a = data[dp+3]" because we don't need the alpha value
+			let p = Colour(r,g,b);
+
+			let mut cidx: u8 = 16;
+			for colour in 0..=15 {
+				let c = &PALETTE[colour];
+				if c == &p {
+					cidx = colour as u8;
+					break;
+				}
+			}
+			if cidx == 16 {
+				panic!("Pixel {} offset {}, colour not in palette: {}", n, dp, p);
+			}
+			converted[n] = cidx;
+		}
+		return converted;
+	}
+	pub fn from_png(input_file_name: String) -> Image {
+		let decoder = png::Decoder::new(File::open(input_file_name).unwrap());
+		let (info, mut reader) = decoder.read_info().unwrap();
+
+		println!("Input image size: {}x{}",info.width,info.height);
+
+		// Allocate the buffer
+		let mut buf = vec![0; info.buffer_size()];
+
+		// Decode image
+		reader.next_frame(&mut buf).unwrap();
+
+		// Make the indexed palette
+		let p_data = Image::to_indexed(&buf);
+		
+		Image {
+			width: info.width,
+			height: info.height,
+			rgba_data: buf,
+			p_data: p_data
+		}
+	}
+	pub fn find_colours(&self) -> Vec<u8> {
+		let mut result: Vec<u8> = vec![];
+		for i in 0..self.p_data.len() {
+			let mut found = false;
+			for c in 0..result.len() {
+				if self.p_data[i] == result[c] {
+					found = true; break;
+				}
+			}
+			if !found {
+				result.push(self.p_data[i]);
+			}
+		}
+		return result;
+	}
+	pub fn match_palette() {
+
+	}
+}
+
--- bitmap_crusher/src/main.rs
+++ bitmap_crusher/src/main.rs
@@ -1,112 +1,6 @@
 
-use std::fs::File;
-
-// TODO: Make the struct fancily comparable with just an ==
-// TODO: Some kind of a function to convert the colour to a string.
-struct Colour(u8,u8,u8);
-struct Mode(u8,u8,u8,u8);
-
-fn compare_colour(a:&Colour,b:&Colour) -> bool {
-	return a.0 == b.0 && a.1 == b.1 && a.2 == b.2;
-}
-
-struct Image {
-	width: u32,
-	height: u32,
-	rgba_data: Vec<u8>
-}
-
-fn load_png(input_file_name: String) -> (u32,u32,Vec<u8>) {
-	let decoder = png::Decoder::new(File::open(input_file_name).unwrap());
-	let (info, mut reader) = decoder.read_info().unwrap();
-
-	println!("Input image size: {}x{}",info.width,info.height);
-
-	// Allocate the buffer
-	let mut buf = vec![0; info.buffer_size()];
-
-	// Decode image
-	reader.next_frame(&mut buf).unwrap();
-
-	return (info.width,info.height,buf);
-}
-
-const PALETTE: [Colour; 16] = [
-	Colour(0x00,0x00,0x00),  // 0 black
-	Colour(0x00,0x00,0xAA),  // 1 blue
-	Colour(0x00,0xAA,0x00),  // 2 green
-	Colour(0x00,0xAA,0xAA),  // 3 cyan
-	Colour(0xAA,0x00,0x00),  // 4 red
-	Colour(0xAA,0x00,0xAA),  // 5 magenta
-	Colour(0xAA,0x55,0x00),  // 6 brown
-	Colour(0xAA,0xAA,0xAA),  // 7 light gray
-	Colour(0x55,0x55,0x55),  // 8 dark gray
-	Colour(0x55,0x55,0xFF),  // 9 light blue
-	Colour(0x55,0xFF,0x55),  // 10 light green
-	Colour(0x55,0xFF,0xFF),  // 11 light cyan
-	Colour(0xFF,0x55,0x55),  // 12 light red
-	Colour(0xFF,0x55,0xFF),  // 13 light magenta
-	Colour(0xFF,0xFF,0x55),  // 14 yellow
-	Colour(0xFF,0xFF,0xFF)   // 15 white
-];
-const MODES: [Mode; 4] = [
-	Mode( 0, 2, 4, 6), // P0Lo: black, green, red, brown 
-	Mode( 0,10,12,14), // P0Hi: black, lt.green, lt.red, yellow
-	Mode( 0, 3, 5, 7), // P1Lo: black, cyan, magenta, lt.gray
-	Mode( 0,11,13,15)  // P1Hi: black, lt.cyan, lt.magenta, white
-];
-enum GraphicsMode {
-	P0LO, P0HI, P1LO, P1HI
-}
-
-fn to_indexed(data: &Vec<u8>) -> Vec<u8> {
-	if data.len() % 4 != 0 {
-		panic!("Data size not divisible by 4 (not RGBA image???)");
-	}
-	let l = data.len()/4;
-	let mut converted = vec![0 as u8; l];
-	for n in 0..l {
-		let dp = n * 4;
-		let ra = data[dp];
-		let ga = data[dp+1];
-		let ba = data[dp+2];
-		let p = Colour(ra,ga,ba);
-
-		let mut cidx: u8 = 16;
-		for colour in 0..=15 {
-			let c = &PALETTE[colour];
-			if compare_colour(c,&p) {
-				cidx = colour as u8;
-				break;
-			}
-		}
-		if cidx == 16 {
-			panic!("Pixel {} offset {}, colour not in palette: #{:02X}{:02X}{:02X}",
-				n, dp, p.0,p.1,p.2);
-		}
-		converted[n] = cidx;
-	}
-	return converted;
-}
-fn find_colours(data: &Vec<u8>) -> Vec<u8> {
-	let mut result: Vec<u8> = vec![];
-	for i in 0..data.len() {
-		let mut found = false;
-		for c in 0..result.len() {
-			if data[i] == result[c] {
-				found = true; break;
-			}
-		}
-		if !found {
-			result.push(data[i]);
-		}
-	}
-	return result;
-}
-fn match_palette() {
-
-}
-
+mod cga_image;
+use cga_image::Image;
 
 fn main() {
 	let arguments = std::env::args();
@@ -125,24 +19,20 @@ fn main() {
 	println!("Input file: {}",input_file);
 	println!("Output file: {}",output_file);
 
-	let decoded = load_png(input_file);
-	let width = decoded.0;
-	let height = decoded.1;
-	let data = decoded.2;
-	println!("{}x{}, {} bytes.", width, height, data.len());
-	let paletted = to_indexed(&data);
-	println!("Paletted image size: {}",paletted.len());
+	let image = Image::from_png(input_file);
+	println!("{}x{}, {} bytes.", image.width, image.height, image.rgba_data.len());
+	println!("Paletted image size: {}",image.p_data.len());
 	if dump_bitmap {
-		for y in 0..height {
-			for x in 0..width {
-				let i = (y*width+x) as usize;
-				let ch = paletted[i];
+		for y in 0..image.height {
+			for x in 0..image.width {
+				let i = (y*image.width+x) as usize;
+				let ch = image.p_data[i];
 				print!("{:02X}",ch);
 			}
 			println!();
 		}
 	}
-	let found_colours = find_colours(&paletted);
+	let found_colours = image.find_colours();
 	println!("Found {} colours: {:?}",found_colours.len(),found_colours);
 	if found_colours.len() > 4 {
 		panic!("Can only deal with 4 colour images.");
